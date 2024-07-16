11d66ada84f647f28e37ea24a70e99b002478ba2
kibiruki <+>
2024-07-16 04:38:03 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solutions in this file
+// index.js
+
+// Function to generate thank you messages for each name provided
+function writeCards(names, event) {
+  const messages = [];
+  for (let i = 0; i < names.length; i++) {
+    messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+  }
+  return messages;
+}
+
+// Function to count down from a given number to zero and log each number
+function countDown(number) {
+  while (number >= 0) {
+    console.log(number);
+    number--;
+  }
+}
+
+module.exports = {
+  writeCards,
+  countDown
+};
